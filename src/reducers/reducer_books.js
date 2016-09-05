68ea0c4339f4301f079c02dd9910304b795f8153
Tsinat Zeree <cheru87@gmail.com>
2016-09-05 13:00:42 -0700

    list of books displayed

---
+++ src/reducers/reducer_books.js
@@ -0,0 +1,8 @@
+export default function() {
+    return [
+        {title: 'Javascript the goog part'},
+        {title: 'Harry potter'},
+        {title: 'The dark tower '},
+        {title: 'Eloquent Ruby'},
+    ]
+}
